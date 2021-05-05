03eb85585ad608c122f64c5a6d503813b5ce6c6c
Audrey Belhoste <audrey.belhoste@gmail.com>
2021-05-05 19:28:24 +0200

    cursor pointer sur liste + alt image photographe

--- js/photographer.js
+++ js/photographer.js
@@ -67,6 +67,7 @@ function showInformations(data){
 			imageContainer.classList.add("presentation__image");
 
 			image.src = 'Sample Photos/Photographers ID Photos/' + photographers[i].portrait;
+			image.setAttribute('alt', photographers[i].name);
 	
 			presentation.appendChild(blocInfo);
 			blocInfo.appendChild(name);
